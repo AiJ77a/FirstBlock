60dbc5117ec7876213a6661eec82e7947049d32f
Aidai <AidaiJk777@gmail.com>
2023-08-24 19:40:24 +0600

    using printf

---
+++ src/Week1/Task1.java
@@ -0,0 +1,15 @@
+package Week1;
+
+public class Task1 {
+    //write a program that will print
+    // out the LAmborgini model; price by decalring a make, model and price variables
+    // and using printf()
+    public static void main(String[] args) {
+
+
+        String make = "lamborghini";
+        String hurancanModel = "huracan";
+        String hurancanPrice = "229k";
+        System.out.printf("%s, %s, %s", make, hurancanModel, hurancanPrice );
+    }
+}
